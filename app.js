5ce53b6b0d822ce0dd32935e8e6bece0ca940b7b
Mattgic <m.lacrampe@novediagroup.com>
2014-03-03 14:10:27 +0100

    Tweaker webapp imprved

--- app.js
+++ app.js
@@ -7,12 +7,12 @@ var hGateway = require("hubiquitus-gateway");
 
 var app = express();
 var botScreenName = 'HubiquitusCalc';
-var botMode = 'forceValue';
+var botMode = 'realCalc';
 var forcedValue = 0;
 var tweakOp = '';
-var answerSentence = "I'm calculating **OP** ... I think it's **RES** !";
+var answerSentence = "I'm calculating the op **OP** ... I think it's **RES** !";
 
-app.use('/twittercalc', express["static"](path.resolve(__dirname, 'web')));
+app.use("/twittercalc", express["static"](path.resolve(__dirname, "web")));
 var httpServer = http.createServer(app);
 
 var gateway = hGateway.createGateway();
@@ -30,25 +30,26 @@ var T = new Twit({
 console.log("=====================================");
 console.log("Hubiquitus Twitter calculator is running");
 console.log("Start the configuration web page now at http://localhost/twittercalc");
-var regex = /([\(-+]?\d*\.?\d+\)?[\/\+\-\*])+([-+]?\d*\.?\d+\)?)/g;
-
-
+var regex = /(?:[\(\-+]?\d*\.?\d+\)?[\/\+\-\*])+(?:[-+]?\d*\.?\d+\)?)/g;
 
 console.log("stream..");
-var stream = T.stream('statuses/filter', { track: '@' + botScreenName })
-stream.on('tweet', function (tweet) {
-    console.log('tweet !', tweet.text);
+var stream = T.stream("statuses/filter", { track: "@" + botScreenName });
+
+stream.on("tweet", function (tweet) {
+    console.log("tweet !", tweet.text);
     if (tweet.user.screen_name != botScreenName) {
-        var string = tweet.text.replace(/\s+/g, '');
+        var string = tweet.text.replace(/\s+/g, "");
         var result = string.match(regex);
         var i = -1;
         while(result[++i] != undefined) {
             var operation = result[i];
             var answerText = "";
-            console.log('mode', botMode);
+            console.log("mode", botMode);
             switch (botMode) {
                 case "realCalc":
-                    var resultValue = eval(operation).toFixed(3);
+                    console.log("op", operation);
+                    var resultValue = Math.round(eval(operation)*1000)/1000;
+                    console.log("op", resultValue);
                     answerText = compileSentence(operation, resultValue);
                     break;
                 case "forceValue" :
@@ -56,13 +57,14 @@ stream.on('tweet', function (tweet) {
                     answerText = compileSentence(operation, resultValue);
                     break;
                 case "tweakValue" :
-                    operation = operation + tweakOp;
+                    var effectiveOp = "(" + operation + ")" + tweakOp;
+                    var resultValue = Math.round(eval(effectiveOp)*1000)/1000;
                     answerText = compileSentence(operation, resultValue);
                     break;
             }
 
             answerText = "@" + tweet.user.screen_name + " " + answerText;
-            T.post('statuses/update', {
+            T.post("statuses/update", {
                     status: answerText,
                     in_reply_to_status_id: tweet.id
                 }, function(err) {
@@ -80,34 +82,41 @@ h.addActor("twitCalcBot", function(req) {
     console.log("received :", req.content);
     switch (req.content.type) {
         case "initCalc":
-            req.reply(null, {mode: botMode, sentence: answerSentence});
+            var initContent = {mode: botMode, sentence: answerSentence};
+            if (tweakOp.length != 0) {
+                initContent.tweakOp = tweakOp
+            }
+            req.reply(null, initContent);
             break;
         case "realCalc" :
             botMode = req.content.type;
+            req.reply();
             break;
         case "forceValue" :
             botMode = req.content.type;
             forcedValue = req.content.val;
+            req.reply();
             break;
-        case "forceAnswer" :
-            botMode = req.content.type;
-            answerSentence = req.content.val;
         case "tweakValue" :
             botMode = req.content.type;
             tweakOp = req.content.op;
+            req.reply();
             break;
+    }
+    if(req.content.sentence) {
+        answerSentence = req.content.sentence;
     }
 }).start();
 
 var calculate = function(text) {
-    var string = text.replace(/\s+/g, '');
+    var string = text.replace(/\s+/g, "");
     var result = string.match(regex);
     var i = -1;
     while(result[++i] != undefined) {
         var res = eval(result[i]).toFixed(3);
         var answerText = "@" + tweet.user.screen_name + " I'm calculating " + result[i] + " ... I think it's " + res + " !";
         console.log(answerText);
-        T.post('statuses/update', {
+        T.post("statuses/update", {
                 status: answerText,
                 in_reply_to_status_id: tweet.id
             }, function(err) {
--- web/scripts/hubiquitus-functions.js
+++ web/scripts/hubiquitus-functions.js
@@ -5,16 +5,15 @@ var hubiquitus = new Hubiquitus({autoReconnect: true});
 var server = window.location.hostname;
 var endpoint = 'http://' + server + '/hubiquitus';
 
-Hubiquitus.logger.enable('*','debug');
-$()
+var tweakOpRegexp = /([\/\+\-\*])([-+]?\d*\.?\d+)/g;
+var loadingDiv = '';
 
+//Hubiquitus.logger.enable('*','debug');
 hubiquitus.on('connect', function() {
     initCalculator();
-    console.log('connected !');
 
 });
 hubiquitus.on('reconnect', function() {
-    console.log('connected !');
     initCalculator();
 
 });
@@ -33,7 +32,6 @@ hubiquitus.connect(endpoint, {username:'twitcounterWeb'});
 var initCalculator = function() {
     hubiquitus.send('twitCalcBot',  {type:'initCalc'}, function(err, res) {
         if (!err) {
-            console.log('RES REC !', res.content);
             switch(res.content.mode) {
                 case 'realCalc' :
                     $('input[name="bot-mode-choice"][value="choice-1"]').prop('checked', true);
@@ -41,46 +39,62 @@ var initCalculator = function() {
                 case 'forceValue' :
                     $('input[name="bot-mode-choice"][value="choice-2"]').prop('checked', true);
                     break;
-                case 'forceAnswer' :
-                    $('input[name="bot-mode-choice"][value="choice-3"]').prop('checked', true);
-                    break;
                 case 'tweakValue' :
                     $('input[name="bot-mode-choice"][value="choice-4"]').prop('checked', true);
                     break;
             }
             $('textarea.customAnswer').val(res.content.sentence);
+            var tweakOp = res.content.tweakOp;
+            if(tweakOp) {
+                var result = tweakOpRegexp.exec(tweakOp);
+                if(result[1] && result[2]) {
+                    $('#select-op').val(result[1]);
+                    $('#op-value').val(result[2]);
+                }
+            }
+            $('.loadingDiv').fadeOut(200, function() {
+                loadingDiv = this;
+                this.remove();
+                $('.calculator').slideDown();
+            });
         }
-    });
-
-    $(".loadingDiv").fadeOut(200, function() {
-        this.remove();
-        $(".calculator").slideDown();
     });
 
 }
 
-$('#launchConfig').click(function() {
-    var choiceVal = $('input[name=bot-mode-choice]:checked', '.calculator').val();
-    var content = "";
-    console.log('send1', choiceVal);
+$('#launchConfig').click(function(e) {
+    e.preventDefault();
+    $(loadingDiv).css('display','');
+    $("#calculator").append(loadingDiv);
 
+    var choiceVal = $('input[name=bot-mode-choice]:checked', '.calculator').val();
+    var content = '';
+    var sentence = $.trim($('textarea.customAnswer').val());
     switch(choiceVal) {
         case 'choice-1' :
-            content = {type:'realCalc'};
+            content = {type:'realCalc', sentence: sentence};
             break;
         case 'choice-2' :
             var val = parseInt($('#forcedValue').val(), 10);
             if (typeof(val) == 'number') {
-                content = {type:'forceValue', val: val};
+                content = {type:'forceValue', val: val, sentence: sentence};
             }
             break;
         case 'choice-3' :
             var op = $('#select-op').val();
             var val = $('#op-value').val();
-            content = {type:'tweakValue', op: ""+op+val};
+            content = {type:'tweakValue', op: ''+op+val, sentence: sentence};
             break;
     }
-    console.log('send', content);
+
     hubiquitus.send('twitCalcBot', content, function(err, res) {
+        if(!err) {
+            $('.loadingDiv').html('<h1>Done !</h1>');
+            setTimeout(function() {
+                $('.loadingDiv').slideUp(400, function() {
+                    this.remove();
+                });
+            }, 2000);
+        }
     });
 });
